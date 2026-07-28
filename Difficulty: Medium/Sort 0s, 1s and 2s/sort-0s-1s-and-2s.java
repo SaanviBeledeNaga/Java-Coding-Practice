127fa825e53d195364d442d38a274091fcdf17a5
Saanvi Belede Naga <saanvibeledenaga@gmail.com>
2026-07-28 19:31:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution 
+{
+    static String sort012(int[] arr) 
+    {
+        Arrays.sort(arr);
+        String ans = Arrays.toString(arr);
+        return ans;
+    }
+}
